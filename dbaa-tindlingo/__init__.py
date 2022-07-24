eb7876e8da8764f40f76b72d9a399ca02633d430
Matt-Stedman <67828580+Matt-Stedman@users.noreply.github.com>
2022-07-24 18:50:19 +0200

    it works!

--- dbaa-tindlingo/__init__.py
+++ dbaa-tindlingo/__init__.py
@@ -1,10 +1,13 @@
 from instagrapi import Client
 import time
+import toml
+import deepl
+
 
 def respond_to_message(msg):
-    print("Sending message")
-    cl.direct_send(f'Returning {msg.text}.', user_ids=[msg.user_id])
-    time.sleep(1)
+    print("Sending message: ")
+    print(f'Returning {translator.translate_text(msg.text, target_lang="FR").text}.')
+    cl.direct_send(f'Returning {translator.translate_text(msg.text, target_lang="FR").text}.', user_ids=[msg.user_id])
 
 
 def get_last_message(user_id, thread):
@@ -18,18 +21,24 @@ def get_active_conversations():
     return users
 
 if __name__ == "__main__":
+    SECRETS = toml.load("dbaa-tindlingo\secrets.toml")
     cl = Client()
-    cl.login("matts.experiments.playground", "fFWvTWH&i84*h?f")
+    cl.login(SECRETS["INSTAGRAM"]["USER"], SECRETS["INSTAGRAM"]["PASS"])
+    
+    translator = deepl.Translator(SECRETS["DEEPL"]["AUTH_KEY"])
 
     users = get_active_conversations()
     while 1:
         threads = cl.direct_threads()
-        for u in users:
-            for t in threads:
-                if u["id"] in (t_u.pk for t_u in t.users):
-                    last_message = get_last_message(u["id"], t)
-                    if last_message.timestamp.timestamp() > u["last_msg_tstamp"]:
-                        respond_to_message(last_message)
-                        u["last_msg_tstamp"] = last_message.timestamp.timestamp()
-
-        time.sleep(1)
+        print(f"Size of threads: {len(threads)}")
+        try:
+            for u in users:
+                for t in threads:
+                    if u["id"] in (t_u.pk for t_u in t.users):
+                        last_message = get_last_message(u["id"], t)
+                        if last_message.timestamp.timestamp() > u["last_msg_tstamp"]:
+                            respond_to_message(last_message)
+                            u["last_msg_tstamp"] = last_message.timestamp.timestamp()
+        except:
+            cl.login("matts.experiments.playground", "fFWvTWH&i84*h?f")
+            users = get_active_conversations()
